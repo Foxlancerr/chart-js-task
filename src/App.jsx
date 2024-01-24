96e8724cffb959ac0b497abe25b554dcd6d7fd00
M-Asimm <kpkprince55persia@gmail.com>
2024-01-24 14:34:32 +0500

    chart interview question solved

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+import React from "react";
+import "./App.css";
+import { LineChart } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LineChart />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+const realData = {
+    "languages": ["JavaScript", "Python", "Java", "C#", "C++"],
+    "metrics": [
+        {
+            "name": "Popularity Index",
+            "data": [85, 78, 70, 65, 60] // Real-time data
+        },
+        {
+            "name": "Usage in Web Development",
+            "data": [90, 68, 80, 30, 42] // Real-time data
+        },
+        {
+            "name": "Community Rating",
+            "data": [4.7, 4.3, 4.6, 4.1, 3.8] // Real-time data
+        },
+        {
+            "name": "Job Market Demand",
+            "data": [80, 58, 74, 35, 48] // Real-time data
+        }
+    ]
+}
+
+export default realData;
