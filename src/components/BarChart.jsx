fbd3b3112f93f2d37313ecb3c1f41043c23cb5a0
M-Asimm <kpkprince55persia@gmail.com>
2024-01-24 19:33:19 +0500

    responsiveness is started

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -31,10 +31,12 @@ const BarChart = () => {
   }, []); // Empty dependency array means this useEffect runs once, like componentDidMount
 
   return (
-    <div>
-      <h2>Language Popularity Comparison</h2>
-      {/* <Line data={chartData} /> */}
-      <Bar data={chartData} />
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[60%] ">
+        <h2 className="text-4xl mb-10 font-bold text-blue-600 text-center">Language Popularity Comparison</h2>
+        {/* <Line data={chartData} /> */}
+        <Bar data={chartData} />
+      </div>
     </div>
   );
 };
